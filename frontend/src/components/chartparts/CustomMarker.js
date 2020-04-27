e82caaf0f2c15129c3de9182637676bc1250209d
Julius Laitala <jiilaitala@gmail.com>
2020-04-27 14:12:17 +0300

    Show pulsar distance via the radius of the central dot instead of via the arm length

--- frontend/src/components/chartparts/CustomMarker.js
+++ frontend/src/components/chartparts/CustomMarker.js
@@ -28,17 +28,21 @@ const getColors = (types, defaultColor) => {
     return [typeToColor[types[0]], typeToColor[types[1]]]
 }
 
+const defaultLength = 15;
 export const minLength = 8;
 export const maxLength = 25;
 export const lineWidth = 2;
 export const lineOpacity = .7;
 export const dotRadius = 2;
+const minRadius = 1.5;
+const maxRadius = 4;
 export const freqScale = 1000;
 export const lengthScale = 25;
 
 export default function CustomMarker({cx, cy, fill, maximums, minimums, payload}) {
     const angle = Math.PI * scale(payload.f0 / maximums.f0, freqScale);
-    const lineLength = minLength + (maxLength - minLength)
+    const lineLength = defaultLength;
+    const radius = minRadius + (maxRadius - minRadius)
         * (1 - scale((payload.dist_dm - minimums.dist_dm) / (maximums.dist_dm - minimums.dist_dm), lengthScale));
     const x = Math.cos(angle) * lineLength;
     const y = -Math.sin(angle) * lineLength;
@@ -47,6 +51,6 @@ export default function CustomMarker({cx, cy, fill, maximums, minimums, payload}
     return <>
         <line x1={cx} y1={cy} x2={cx + lineLength} y2={cy} stroke={clr0} strokeWidth={lineWidth} strokeOpacity={lineOpacity} />
         <line x1={cx} y1={cy} x2={cx + x} y2={cy + y} stroke={clr1} strokeWidth={lineWidth} strokeOpacity={lineOpacity} />
-        <circle cx={cx} cy={cy} r={dotRadius} fill={fill} />
+        <circle cx={cx} cy={cy} r={radius} fill={fill} />
     </>
 }
